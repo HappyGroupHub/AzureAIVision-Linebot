46d85c867ac52e1eb51f3a711710cdd6ef27a876
LD <chiyc88@gmail.com>
2023-11-02 17:36:36 +0800

    Optimize - optimized codes

--- app.py
+++ app.py
@@ -20,22 +20,13 @@ result = image_analyzer.analyze()
 
 if result.reason == ai_vision.ImageAnalysisResultReason.ANALYZED:
     if result.caption is not None:
-        print(" Caption:")
-        print("   '{}', Confidence {:.4f}".format(result.caption.content, result.caption.confidence))
-
-    if result.text is not None:
-        print(" Text:")
-        for line in result.text.lines:
-            points_string = "{" + ", ".join([str(int(point)) for point in line.bounding_polygon]) + "}"
-            print("   Line: '{}', Bounding polygon {}".format(line.content, points_string))
-            for word in line.words:
-                points_string = "{" + ", ".join([str(int(point)) for point in word.bounding_polygon]) + "}"
-                print("     Word: '{}', Bounding polygon {}, Confidence {:.4f}"
-                      .format(word.content, points_string, word.confidence))
-
+        caption = result.caption.content
+        confidence = result.caption.confidence
+        print(f"Caption: {caption}")
+        print(f"Confidence: {confidence}")
 else:
     error_details = ai_vision.ImageAnalysisErrorDetails.from_result(result)
-    print(" Analysis failed.")
-    print("   Error reason: {}".format(error_details.reason))
-    print("   Error code: {}".format(error_details.error_code))
-    print("   Error message: {}".format(error_details.message))
+    print("Analysis failed.")
+    print(f"   Error reason: {error_details.reason}")
+    print(f"   Error code: {error_details.error_code}")
+    print(f"   Error message: {error_details.message}")
